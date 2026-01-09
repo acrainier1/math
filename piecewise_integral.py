589c4dcc9065552f0d3f181fb6cd08961e80cd6e
acrainier1 <alex.c.reinard@gmail.com>
2026-01-08 21:01:40 -0500

    update

--- piecewise_integral.py
+++ piecewise_integral.py
@@ -12,11 +12,57 @@ def PolyCoefficients(x, coefficents):
     for i in range(order):
         coefficent = coefficents[i]
         y += coefficent * (x ** i)
-  
+        # print('x:', x)
+
     return y
 
-x = np.linspace(-100, 100, 100)
+def AntiDerivativePolyCoefficients(x, coefficents, x_i, width):
+    y = PolyCoefficients(x, coefficents)
+    return y
+
+
+# FUNCTION
+x_domain = np.linspace(-20, 20, 100000)
 coefficents = [0, 0, 1]
 
-plt.plot(x, PolyCoefficients(x, coefficents))
-plt.show()
+
+# INTEGRAL
+
+
+# INTEGRAL PLOT
+fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
+
+for c in range(-1, 2, 1):
+    x_i = 0
+    width = 1.0
+    integral_piece = AntiDerivativePolyCoefficients(x_domain, [c, 1, 0], x_i, width)
+    ax1.plot(x_domain, integral_piece, color='#FFA500')
+
+# FUNCTION PLOT
+f_function = PolyCoefficients(x_domain, coefficents)
+print('func:', f_function)
+ax2.plot(x_domain, f_function)
+
+plt.ylim(-10, 10)
+plt.tight_layout()
+# plt.show()
+
+x_test = np.linspace(-1, 1, 100)
+y = 0
+for i in range(3):
+    y += 5 * (x_test ** 2)
+
+# print('test:', x_test)
+# print('\n===================\ny:', y)
+
+sub_domain = 0
+sub_domain += 5 * (x_test ** 2)
+c=0
+
+for i, x in enumerate(sub_domain):
+    if x < 0 or x >= 1:
+        sub_domain[i] = 0
+        c+=1
+
+print(len(x_test), len(sub_domain), c)
+print('\n===================\nsub_domain:', sub_domain)
